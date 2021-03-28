597c68c84cb58d1b3dc8554fd1564f21a7dec7cb
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-03-28 23:09:01 +0100

    Added custom http fetch hook

---
+++ src/shared/hooks/http-axios-hook.jsx
@@ -0,0 +1,3 @@
+import { useCallback, useState, useEffect } from "react";
+// would need to import and install axios to do it. But will be much easier to cleaup
+export const useHttpAxiosClien = () => {};
---
+++ src/shared/hooks/http-fetch-hook.jsx
@@ -0,0 +1,51 @@
+import { useCallback, useState, useEffect, useCallback, useRef } from "react";
+
+export const useHttpFetchClien = () => {
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
+
+  const activeHttpRequest = useRef([]); // store data accross re-render cycles
+
+  const sendRequest = useCallback(async (url, method = "GET", body = null, headers = {}) => {
+    setIsLoading(true);
+    // cancel request stuff for fetch
+    const httpAbortController = new AbortController();
+    activeHttpRequest.current.push(httpAbortController);
+    try {
+      const response = await fetch(url, {
+        method,
+        body,
+        headers,
+        signal: httpAbortController.signal, // cancel request stuff for fetch
+      });
+      const responseData = await response.json();
+
+      // clearing old requests for fetch... God I miss Axios
+      activeHttpRequest.current = activeHttpRequest.current.filter(
+        reqController => reqController !== httpAbortController
+      );
+
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+
+      return responseData;
+      setIsLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setIsLoading(false);
+      throw err;
+    }
+  }, []);
+
+  const clearError = () => {
+    setError(null);
+  };
+  useEffect(() => {
+    // cleanup
+    return () => {
+      activeHttpRequest.current.forEach(httpAbortController => httpAbortController.abort());
+    };
+  }, []);
+  return { isLoading, error, sendRequest, clearError };
+};
