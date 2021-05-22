d3c3289b445c69537bb4a2e8a40e7405ad4991bc
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-05-22 21:05:33 +0100

    changed text

--- src/places/pages/FindPlace.jsx
+++ src/places/pages/FindPlace.jsx
@@ -1,18 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
-import { useHistory } from "react-router-dom";
 import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 import { useHttpFetchClient } from "../../shared/hooks/http-fetch-hook";
 import ErrorModal from "../../shared/components/UIElements/LoadingError/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingError/LoadingSpinner";
-import ImageUpload from "../../shared/components/FormElements/ImageUpload";
-import AuthContext from "../../shared/context/auth-context";
 import "./PlaceForm.css";
 
 const NewPlace = props => {
-  const authStateContext = useContext(AuthContext);
   const [loadedPlaces, setLoadedPlaces] = useState();
   const { isLoading, error, sendRequest, clearError } = useHttpFetchClient();
   const [formState, inputHandler] = useForm(
@@ -24,7 +20,6 @@ const NewPlace = props => {
     },
     false
   );
-  const history = useHistory();
 
   const searchSubmitHandler = async event => {
     event.preventDefault();
@@ -36,7 +31,7 @@ const NewPlace = props => {
         `${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`
       );
       setLoadedPlaces(responseData.places);
-      history.push("/");
+
       //redirect to a different page
     } catch (e) {
       console.log(`Couldn't send request ${e}`);
@@ -62,7 +57,7 @@ const NewPlace = props => {
           onInput={inputHandler}
         />
         <Button type="submit" disabled={!formState.isValid}>
-          Show me where I can go..
+          Search..
         </Button>
       </form>
     </>
